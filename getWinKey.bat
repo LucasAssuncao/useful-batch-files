c29146c5a9bce9d2ad79fd243eb8b35ed63698a9
Lucas Assunção da Silva <lucas.9592@gmail.com>
2017-06-22 13:05:49 -0300

    Upload getWinKey.bat
    Batch file to GET original Windows Activation key and store into a log file.

--- getWinKey.bat
+++ getWinKey.bat
@@ -1,5 +1,10 @@
 @echo off
-echo.
-echo ================================= >> .\TXT\GetWinKey.log
-hostname >> .\TXT\GetWinKey.log
-wmic path softwarelicensingservice get OA3xOriginalProductKey | tr -d "\r\n\b" | findrepl "         " " " >> .\TXT\GetWinKey.log
+
+set _SCRIPT_PATH=%~p0
+set _SCRIPT_DRIVE=%~d0
+set _SCRIPT_OUTFILE=%_SCRIPT_DRIVE%%_SCRIPT_PATH%TXT\GetWinKey.log
+
+echo =====================================================>>%_SCRIPT_OUTFILE%
+hostname>>%_SCRIPT_OUTFILE%
+wmic path softwarelicensingservice get OA3xOriginalProductKey | tr -d "\r\n\b" | findrepl "         " " ">>%_SCRIPT_OUTFILE%
+echo.>>%_SCRIPT_OUTFILE%
